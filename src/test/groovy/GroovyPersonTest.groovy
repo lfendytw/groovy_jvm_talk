e531f86cf13ab87d8170d94c244c1fa12ee3917b
Liauw Fendy <lfendy@gmail.com>
2015-08-02 00:06:37 +1000

    More idiomatic assertion

--- src/test/groovy/GroovyPersonTest.groovy
+++ src/test/groovy/GroovyPersonTest.groovy
@@ -1,18 +1,17 @@
 import org.junit.Test
-import static org.junit.Assert.*
 
 class GroovyPersonTest {
     @Test void personShouldHaveName() {
         GroovyPerson toTest = new GroovyPerson("John", "don't care", "don't care")
-        assertEquals("instantiated Person should have name", toTest.name, "John")
+        assert toTest.name == "John", "instantiated Person should have name"
     }
     @Test void personShouldHaveDob() {
         GroovyPerson toTest = new GroovyPerson("don't care", "14-12-1988", "don't care")
-        assertEquals("instantiated Person should have dob", toTest.dob, "14-12-1988")
+        assert toTest.dob == "14-12-1988", "instantiated Person should have dob"
     }
     @Test void personShouldHaveGender() {
         GroovyPerson toTest = new GroovyPerson("don't care", "don't care", "M")
-        assertEquals("instantiated Person should have gender", toTest.gender, "M")
+        assert toTest.gender == "M", "instantiated Person should have gender"
     }
 }
 
