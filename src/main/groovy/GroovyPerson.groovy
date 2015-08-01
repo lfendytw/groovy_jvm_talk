8e2b1639eac37d1672953932f5ae2fba83cd6eb3
Liauw Fendy <lfendy@gmail.com>
2015-08-01 23:39:53 +1000

    remove semi-colon from groovy person and test

--- src/main/groovy/GroovyPerson.groovy
+++ src/main/groovy/GroovyPerson.groovy
@@ -1,24 +1,24 @@
 public class GroovyPerson {
-  private String name;
-  private String dob;
-  private String gender;
+  private String name
+  private String dob
+  private String gender
 
   public GroovyPerson(String name, String dob, String gender){
-    this.name = name;
-    this.dob = dob;
-    this.gender = gender;
+    this.name = name
+    this.dob = dob
+    this.gender = gender
   }
 
   public String getName(){
-    return this.name;
+    return this.name
   }
 
   public String getDob(){
-    return this.dob;
+    return this.dob
   }
 
   public String getGender(){
-    return this.gender;
+    return this.gender
   }
 }
 
--- src/test/groovy/GroovyPersonTest.groovy
+++ src/test/groovy/GroovyPersonTest.groovy
@@ -1,18 +1,18 @@
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.Test
+import static org.junit.Assert.*
 
 public class GroovyPersonTest {
     @Test public void personShouldHaveName() {
-        GroovyPerson toTest = new GroovyPerson("John", "don't care", "don't care");
-        assertEquals("instantiated Person should have name", toTest.getName(), "John");
+        GroovyPerson toTest = new GroovyPerson("John", "don't care", "don't care")
+        assertEquals("instantiated Person should have name", toTest.getName(), "John")
     }
     @Test public void personShouldHaveDob() {
-        GroovyPerson toTest = new GroovyPerson("don't care", "14-12-1988", "don't care");
-        assertEquals("instantiated Person should have dob", toTest.getDob(), "14-12-1988");
+        GroovyPerson toTest = new GroovyPerson("don't care", "14-12-1988", "don't care")
+        assertEquals("instantiated Person should have dob", toTest.getDob(), "14-12-1988")
     }
     @Test public void personShouldHaveGender() {
-        GroovyPerson toTest = new GroovyPerson("don't care", "don't care", "M");
-        assertEquals("instantiated Person should have gender", toTest.getGender(), "M");
+        GroovyPerson toTest = new GroovyPerson("don't care", "don't care", "M")
+        assertEquals("instantiated Person should have gender", toTest.getGender(), "M")
     }
 }
 
