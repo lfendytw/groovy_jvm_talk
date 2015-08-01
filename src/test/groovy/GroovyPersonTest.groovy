b1c48245e5e66b956f0c9e0f0b351803849ed48a
Liauw Fendy <lfendy@gmail.com>
2015-08-01 23:48:01 +1000

    remove explicit getter from GroovyPersonTest

--- src/test/groovy/GroovyPersonTest.groovy
+++ src/test/groovy/GroovyPersonTest.groovy
@@ -4,15 +4,15 @@ import static org.junit.Assert.*
 class GroovyPersonTest {
     @Test void personShouldHaveName() {
         GroovyPerson toTest = new GroovyPerson("John", "don't care", "don't care")
-        assertEquals("instantiated Person should have name", toTest.getName(), "John")
+        assertEquals("instantiated Person should have name", toTest.name, "John")
     }
     @Test void personShouldHaveDob() {
         GroovyPerson toTest = new GroovyPerson("don't care", "14-12-1988", "don't care")
-        assertEquals("instantiated Person should have dob", toTest.getDob(), "14-12-1988")
+        assertEquals("instantiated Person should have dob", toTest.dob, "14-12-1988")
     }
     @Test void personShouldHaveGender() {
         GroovyPerson toTest = new GroovyPerson("don't care", "don't care", "M")
-        assertEquals("instantiated Person should have gender", toTest.getGender(), "M")
+        assertEquals("instantiated Person should have gender", toTest.gender, "M")
     }
 }
 
