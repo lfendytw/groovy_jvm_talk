5ff4028ce9e3de1c683acd6f969bd6b298a30350
Liauw Fendy <lfendy@gmail.com>
2015-08-01 23:44:52 +1000

    remove access modifier from test

--- src/test/groovy/GroovyPersonTest.groovy
+++ src/test/groovy/GroovyPersonTest.groovy
@@ -1,16 +1,16 @@
 import org.junit.Test
 import static org.junit.Assert.*
 
-public class GroovyPersonTest {
-    @Test public void personShouldHaveName() {
+class GroovyPersonTest {
+    @Test void personShouldHaveName() {
         GroovyPerson toTest = new GroovyPerson("John", "don't care", "don't care")
         assertEquals("instantiated Person should have name", toTest.getName(), "John")
     }
-    @Test public void personShouldHaveDob() {
+    @Test void personShouldHaveDob() {
         GroovyPerson toTest = new GroovyPerson("don't care", "14-12-1988", "don't care")
         assertEquals("instantiated Person should have dob", toTest.getDob(), "14-12-1988")
     }
-    @Test public void personShouldHaveGender() {
+    @Test void personShouldHaveGender() {
         GroovyPerson toTest = new GroovyPerson("don't care", "don't care", "M")
         assertEquals("instantiated Person should have gender", toTest.getGender(), "M")
     }
