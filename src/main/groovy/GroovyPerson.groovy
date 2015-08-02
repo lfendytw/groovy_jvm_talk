7abea6b614f492b95036055b87a5a8fab853a4a4
Liauw Fendy <lfendy@gmail.com>
2015-08-02 19:34:11 +1000

    start adding location map into groovyPerson verbatim

--- src/main/groovy/GroovyPerson.groovy
+++ src/main/groovy/GroovyPerson.groovy
@@ -1,9 +1,19 @@
 class GroovyPerson {
-   String name
-   String dob
-   String gender
+  String name
+  String dob
+  String gender
+  private Map<String, Integer> timesVisited;
 
    GroovyPerson() {}
+
+  public void addLocationVisited(String location){
+    timesVisited.put(location,timesVisitedLocation(location)+1);
+  }
+
+  public int timesVisitedLocation(String location){
+    Integer toReturn = timesVisited.get(location);
+    return toReturn == null ? 0 : toReturn;
+  }
 
 }
 
--- src/test/groovy/GroovyPersonTest.groovy
+++ src/test/groovy/GroovyPersonTest.groovy
@@ -1,4 +1,5 @@
 import org.junit.Test
+import static org.junit.Assert.*;
 
 class GroovyPersonTest {
 
@@ -11,6 +12,28 @@ class GroovyPersonTest {
         assert toTest[prop] == value, "instantiated Person should have ${prop}"
       })
     }
+
+
+    @Test public void personLocationVisitedShouldIncreaseWithNewAdditions() {
+        Person toTest = new Person("","","");
+        String location = "Some Location";
+        toTest.addLocationVisited(location);
+        assertEquals("new visited location should be remembered", toTest.timesVisitedLocation(location), 1);
+        toTest.addLocationVisited(location);
+        assertEquals("previously visited location should be increase", toTest.timesVisitedLocation(location), 2);
+    }
+    @Test public void personLocationVisitedShouldStartWithZero() {
+        Person toTest = new Person("","","");
+        assertEquals("never visited location should return zero", toTest.timesVisitedLocation("New Location"), 0);
+    }
+    @Test public void personLocationVisitedShouldBePrepopulated() {
+        Person toTest = new Person("","","");
+        assertEquals("Melbourne - 5", toTest.timesVisitedLocation("Melbourne"), 5);
+        assertEquals("Sydney - 2", toTest.timesVisitedLocation("Sydney"), 2);
+        assertEquals("Brisbane - 7", toTest.timesVisitedLocation("Brisbane"), 7);
+        assertEquals("Adelaide - 3", toTest.timesVisitedLocation("Adelaide"), 3);
+    }
+
 }
 
 
