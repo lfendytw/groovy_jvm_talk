0927ea76a1f36231c6b05d0b048989d20759a92e
Liauw Fendy <lfendy@gmail.com>
2015-08-02 19:28:34 +1000

    Person Java -- with LocationVisited map

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,12 +1,29 @@
+import java.util.Map;
+import java.util.HashMap;
+
 public class Person {
   private String name;
   private String dob;
   private String gender;
+  private Map<String, Integer> timesVisited;
 
   public Person(String name, String dob, String gender){
     this.name = name;
     this.dob = dob;
     this.gender = gender;
+    this.timesVisited = new HashMap<String, Integer>();
+    this.timesVisited.put("Melbourne",5);
+    this.timesVisited.put("Sydney",2);
+    this.timesVisited.put("Brisbane",7);
+    this.timesVisited.put("Adelaide",3);
+    /*
+    this.timesVisited = new HashMap<String, Integer>(){{
+      put("Melbourne",5);
+      put("Sydney",2);
+      put("Brisbane",7);
+      put("Adelaide",3);
+    }};
+     * */
   }
 
   public String getName(){
@@ -20,4 +37,14 @@ public class Person {
   public String getGender(){
     return this.gender;
   }
+
+  public void addLocationVisited(String location){
+    timesVisited.put(location,timesVisitedLocation(location)+1);
+  }
+
+  public int timesVisitedLocation(String location){
+    Integer toReturn = timesVisited.get(location);
+    return toReturn == null ? 0 : toReturn;
+  }
+
 }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -14,5 +14,25 @@ public class PersonTest {
         Person toTest = new Person("don't care", "don't care", "M");
         assertEquals("instantiated Person should have gender", toTest.getGender(), "M");
     }
+
+    @Test public void personLocationVisitedShouldIncreaseWithNewAdditions() {
+        Person toTest = new Person("","","");
+        String location = "Some Location";
+        toTest.addLocationVisited(location);
+        assertEquals("new visited location should be remembered", toTest.timesVisitedLocation(location), 1);
+        toTest.addLocationVisited(location);
+        assertEquals("previously visited location should be increase", toTest.timesVisitedLocation(location), 2);
+    }
+    @Test public void personLocationVisitedShouldStartWithZero() {
+        Person toTest = new Person("","","");
+        assertEquals("never visited location should return zero", toTest.timesVisitedLocation("New Location"), 0);
+    }
+    @Test public void personLocationVisitedShouldBePrepopulated() {
+        Person toTest = new Person("","","");
+        assertEquals("Melbourne - 5", toTest.timesVisitedLocation("Melbourne"), 5);
+        assertEquals("Sydney - 2", toTest.timesVisitedLocation("Sydney"), 2);
+        assertEquals("Brisbane - 7", toTest.timesVisitedLocation("Brisbane"), 7);
+        assertEquals("Adelaide - 3", toTest.timesVisitedLocation("Adelaide"), 3);
+    }
 }
 
