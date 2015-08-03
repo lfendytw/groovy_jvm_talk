2ad25c8d506e0cb5e0c96ad1f9eaa96e8941c34e
John Fulton <jfulton@thoughtworks.com>
2015-08-03 23:57:05 +1000

    Tidied up some formatting

--- src/main/groovy/com/example/GroovyUtils.groovy
+++ src/main/groovy/com/example/GroovyUtils.groovy
@@ -11,16 +11,16 @@ class GroovyUtils {
   }
 
   GroovyPerson findJim(List<GroovyPerson> people) {
-    people.find { it.name == "James Kirk"  }
+    people.find { it.name == "James Kirk" }
   }
 
   Integer doTheOverload(Object o) {
-    System.out.println("Hey, I'm the object version of the method");
+    System.out.println("Hey, I'm the object version of the method")
     1
   }
 
   Integer doTheOverload(String s) {
-    System.out.println("Hey, I'm the string version of the method");
+    System.out.println("Hey, I'm the string version of the method")
     2
   }
 
